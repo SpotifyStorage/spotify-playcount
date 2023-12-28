c5316d619eaf2bd1256faa8f2b1e05c14da3ca67
Mathieu <mathieuducharme86@gmail.com>
2023-12-27 22:29:48 -0500

    removed missing import

--- src/token_handler/token.module.ts
+++ src/token_handler/token.module.ts
@@ -3,11 +3,10 @@ import { HttpModule } from '@nestjs/axios';
 import { TokenController } from './token.controller';
 import { TokenService } from './token.service';
 import { SpotifyController } from 'src/spotify/spotify.controller';
-import { DatabaseController } from 'src/database/database.controler';
 
 @Module({
     imports: [HttpModule],
-    controllers: [TokenController, SpotifyController, DatabaseController],
+    controllers: [TokenController, SpotifyController],
     providers: [TokenService]
 })
 export class TokenModule {}
